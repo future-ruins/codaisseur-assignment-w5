56707532c5ce89d0ec039b1354017f66682ba9c5
future-ruins <helliot885@gmail.com>
2019-09-28 00:28:57 +0200

    Add steps 1-4 in section 1

---
+++ messages-api.js
@@ -0,0 +1,29 @@
+// 1. Create an Express app with a single end-point.
+
+// Create a new JS file named messages-api.js.
+
+// Create an Express app in that file. The app should listen for requests
+//on port 3000.
+
+// Add a single endpoint to the app that responds
+//to POST requests to the /messages URI(Uniform Resource Identifier).
+
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
+
+const port = 3000;
+
+function onListen() {
+  console.log(`Listening on port ${port}`);
+}
+
+app.listen(port, onListen);
+
+app.post("/messages", (request, response) => {
+  console.log("body text:", request.body.text);
+  return response.send({ message: "Message received loud and clear" });
+});
