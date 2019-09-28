3afd6dd5c1d4ef448b377dab1c1e68d2fd59a1c1
future-ruins <helliot885@gmail.com>
2019-09-28 15:49:23 +0200

    Section 2 step 6 add sample data rows

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -25,10 +25,33 @@ const Movie = db.define(
   { timestamps: false }
 );
 
-db.sync()
-  .then(() => console.log('Tables created successfully'))
+db.sync({ force: false })
+  .then(() => console.log('Tables updated successfully'))
+  .then(() =>
+    Movie.bulkCreate([
+      {
+        title: 'Apocalypse Now',
+        yearOfRelease: 1979,
+        synopsis:
+          'A U.S. Army officer serving in Vietnam is tasked with assassinating a renegade Special Forces Colonel who sees himself as a god.',
+      },
+      {
+        title: 'Blade Runner',
+        yearOfRelease: 1982,
+        synopsis:
+          'A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.',
+      },
+      {
+        title: 'Metropolis',
+        yearOfRelease: 1927,
+        synopsis:
+          "In a futuristic city sharply divided between the working class and the city planners, the son of the city's mastermind falls in love with a working class prophet who predicts the coming of a savior to mediate their differences",
+      },
+    ])
+  )
+  .then(result => console.log(result))
   .catch(err => {
-    console.error('Unable to create tables, shutting down...', err);
+    console.error('Unable to create tables', err);
     // exit with failure code if not created
     process.exit(1);
   });
