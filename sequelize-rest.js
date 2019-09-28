929aca22adf1ef5166dfa9b2877f6508dada3d3a
future-ruins <helliot885@gmail.com>
2019-09-28 15:11:48 +0200

    Create model, sync database, remove timestamps

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -1,29 +1,34 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
 const connectionString =
   process.env.DATABASE_URL ||
-  "postgresql://postgres:password@localhost:5432/postgres";
+  'postgresql://postgres:password@localhost:5432/postgres';
 
 const db = new Sequelize(connectionString);
 
-const Movie = db.define("movie", {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
+const Movie = db.define(
+  'movie',
+  {
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    yearOfRelease: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+    },
+    synopsis: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
   },
-  yearOfRelease: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  synopsis: {
-    type: Sequelize.STRING,
-    allowNull: false
-  }
-});
+  { timestamps: false }
+);
 
 db.sync()
-  .then(() => console.log("Tables created successfully"))
+  .then(() => console.log('Tables created successfully'))
   .catch(err => {
-    console.error("Unable to create tables, shutting down...", err);
+    console.error('Unable to create tables, shutting down...', err);
+    // exit with failure code if not created
     process.exit(1);
   });
