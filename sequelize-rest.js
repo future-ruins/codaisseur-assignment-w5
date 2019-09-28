7060244c0d9f0f5d388abe8dfc026e48becc8ca1
future-ruins <helliot885@gmail.com>
2019-09-28 17:50:48 +0200

    Section2 step7 add routes

--- sequelize-rest.js
+++ sequelize-rest.js
@@ -1,4 +1,10 @@
+//Imports
+
 const Sequelize = require('sequelize');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Sequelize and db models set-up
 
 const connectionString =
   process.env.DATABASE_URL ||
@@ -15,19 +21,40 @@ const Movie = db.define(
     },
     yearOfRelease: {
       type: Sequelize.INTEGER,
-      allowNull: false,
+      //allowNull: false,
     },
     synopsis: {
       type: Sequelize.STRING,
-      allowNull: false,
+      //allowNull: false,
     },
   },
   { timestamps: false }
 );
 
-db.sync({ force: false })
+db.sync({ force: true })
   .then(() => console.log('Tables updated successfully'))
   .then(() =>
+    //     //     Promise.all([
+    //     //       Movie.create({
+    //     //         title: 'Apocalypse Now',
+    //     //         yearOfRelease: 1979,
+    //     //         synopsis:
+    //     //           'A U.S. Army officer serving in Vietnam is tasked with assassinating a renegade Special Forces Colonel who sees himself as a god.',
+    //     //       }),
+    //     //       Movie.create({
+    //     //         title: 'Blade Runner',
+    //     //         yearOfRelease: 1982,
+    //     //         synopsis:
+    //     //           'A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.',
+    //     //       }),
+    //     //       Movie.create({
+    //     //         title: 'Metropolis',
+    //     //         yearOfRelease: 1927,
+    //     //         synopsis:
+    //     //           "In a futuristic city sharply divided between the working class and the city planners, the son of the city's mastermind falls in love with a working class prophet who predicts the coming of a savior to mediate their differences",
+    //     //       }),
+    //     //     ])
+    //     //   )
     Movie.bulkCreate([
       {
         title: 'Apocalypse Now',
@@ -49,9 +76,63 @@ db.sync({ force: false })
       },
     ])
   )
-  .then(result => console.log(result))
+
+  //.then(result => console.log(result))
   .catch(err => {
     console.error('Unable to create tables', err);
     // exit with failure code if not created
     process.exit(1);
   });
+
+// Express app, routing, RESTful actions set-up
+// All in same file so I don't need express router I think
+
+const app = express();
+
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
+
+const port = process.env.PORT || 4000;
+
+function onListen() {
+  console.log(`Listening on :${port}`);
+}
+
+app.listen(port, onListen);
+
+// Read all movies
+
+app.get('/movies', (request, response, next) => {
+  Movie.findAll()
+    //.then(console.log(response.body))
+    .then(movies => {
+      //console.log(movies);
+      if (movies.length === 0) {
+        return response.status(404).send({ message: 'Movies not found' });
+      } else {
+        return response.send(movies);
+      }
+    })
+    .catch(next);
+});
+
+// Create a new movie resource
+app.post('/movies', (request, response, next) =>
+  //console.log('request', request.body)
+  Movie.create(request.body)
+    .then(result => {
+      //console.log('MOVIE CREATED', result.dataValues);
+      return response.status(201).send(result);
+    })
+    .catch(error => {
+      if (error.name === 'SequelizeValidationError') {
+        return response
+          .status(422)
+          .send({ message: 'Movie title cannot be null' });
+      } else {
+        return response.status(400).send({ message: 'Bad request' });
+      }
+    })
+);
+
+// Read a single movie resource
